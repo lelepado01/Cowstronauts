cb60281773cdd5740b76334c1f5891b77a1bd8d1
Gabriele Padovani <gabriele.padovani@gmail.com>
2023-06-12 13:33:39 +0200

    missed file

--- src/cow_tils.rs
+++ src/cow_tils.rs
@@ -8,17 +8,3 @@ pub struct CowTag;
 pub struct CowMomentum{
     pub velocity: Vec3,
 }
-
-#[derive(Resource)]
-pub struct MilkSpriteResource{
-    pub milk_sprite: Handle<Image>,
-}
-
-#[derive(Component)]
-pub struct MilkTag;
-
-#[derive(Component)]
-pub struct MilkMomentum {
-    pub velocity : Vec3,
-    pub rotation: Quat,
-}
